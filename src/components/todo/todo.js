5569896fe0e7759e62da35e90c2b0bd768dcf8d0
Chattray <chattray.chea@gmail.com>
2022-05-02 16:46:57 -0700

    gh pages deploy and context usage

---
+++ src/components/todo/todo.js
@@ -0,0 +1,76 @@
+import React from "react";
+import useForm from "../../hooks/form.js";
+
+import { v4 as uuid } from "uuid";
+
+const ToDo = (props) => {
+  const { handleChange, handleSubmit } = useForm(addItem);
+
+  function addItem(item) {
+    item.id = uuid();
+    item.complete = false;
+    const dup = props.list.filter((todo) => {
+      if (todo.text.toLowerCase() === item.text.toLowerCase()) {
+        if (todo.assignee.toLowerCase() === item.assignee.toLowerCase()) {
+          return true;
+        }
+      }
+      return false;
+    });
+    if (dup.length > 0) {
+      return;
+    }
+    props.setList([...props.list, item]);
+  }
+
+  function deleteItem(id) {
+    const items = props.list.filter((item) => item.id !== id);
+    props.setList(items);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h2>Add To Do Item</h2>
+
+        <label>
+          <span>To Do Item</span>
+          <input
+            onChange={handleChange}
+            name="text"
+            type="text"
+            placeholder="Item Details"
+          />
+        </label>
+
+        <label>
+          <span>Assigned To</span>
+          <input
+            onChange={handleChange}
+            name="assignee"
+            type="text"
+            placeholder="Assignee Name"
+          />
+        </label>
+
+        <label>
+          <span>Difficulty</span>
+          <input
+            onChange={handleChange}
+            defaultValue={3}
+            type="range"
+            min={1}
+            max={5}
+            name="difficulty"
+          />
+        </label>
+
+        <label>
+          <button type="submit">Add Item</button>
+        </label>
+      </form>
+    </>
+  );
+};
+
+export default ToDo;
---
+++ src/context/Settings.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+export const SettingsContext = React.createContext();
+
+function Settings(props) {
+  let [itemPref, setItemPref] = useState(5);
+  let [dispComp, setDisComp] = useState(true);
+
+  useEffect(() => {
+    let item = JSON.parse(window.localStorage.getItem("settings")) || {};
+    if (Object.keys(item).length === 0) {
+      console.log("Not Items");
+    } else {
+      console.log("This", item);
+      setItemPref(item.itemPref);
+      setDisComp(item.dispComp);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem(
+      "settings",
+      JSON.stringify({ itemPref, dispComp })
+    );
+  }, [itemPref, dispComp]);
+
+  let val = {
+    dispComp,
+    setDisComp: function () {
+      setDisComp(!dispComp);
+    },
+    itemPref,
+    setItemPref: function (count) {
+      setItemPref(count);
+    },
+    sort: "",
+  };
+
+  return (
+    <SettingsContext.Provider value={val}>
+      {props.children}
+    </SettingsContext.Provider>
+  );
+}
+
+export default Settings;
